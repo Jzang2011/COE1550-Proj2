b706001f35d7e22159ef81a67d48809ba2832def
Jeremy Zang <zang.jeremy@gmail.com>
2018-10-16 14:44:52 -0400

    added initializers for simulation. initialized pointers based on pointer arithmetic from the main pointer returns from mmap(). Changed car queues to be pointers.

--- trafficsim.c
+++ trafficsim.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/mman.h>
 #include <unistd.h> //this should use the one in linux-2.6.23.1
 
 /**
@@ -22,19 +23,23 @@ typedef struct Car {
 } Car;
 
 //PID's for processes.
-int flag_person_pid;
-int north_bound_pid;
-int south_bound_pid;
+int* flag_person_pid = NULL;
+int* north_bound_pid = NULL;
+int* south_bound_pid = NULL;
 
-Car south_bound_queue[10] = {0,0,0,0,0,0,0,0,0,0};
-Car north_bound_queue[10] = {0,0,0,0,0,0,0,0,0,0};
+//Car south_bound_queue[10] = {0,0,0,0,0,0,0,0,0,0};
+//Car north_bound_queue[10] = {0,0,0,0,0,0,0,0,0,0};
+Car* south_bound_queue;
+Car* north_bound_queue;
+
 
 Car* north_bound_first;
 Car* south_bount_first;
 Car* north_bound_last;
 Car* south_bount_last;
-int north_bound_size;
-int south_bound_size;
+int* north_bound_size;
+int* south_bound_size;
+struct cs1550_sem* sem;
 
 
 /**
@@ -61,6 +66,33 @@ void up(cs1550_sem *sem) {
     syscall(__NR_cs1550_up, sem);
 }
 
+/**
+ * Calculates the size of the memory needed to be mapped.
+ * @return N - the number of bytes.
+ */
+int calculate_mem_size() {
+    int N = 0;
+    N = N + (sizeof(int) * 5); //add the size of 5 ints (3 for process ids and 2 for size of queues)
+    N = N + (sizeof(Car) * 24); // 10 for each queue (20 total) + 2 for the head and tail pointers (4 total)
+    N = N + sizeof(cs1550_sem);
+    return N;
+}
+
+void init_ptrs(void* ptr_to_mem) {
+    int sizeOfCar = sizeof(Car);
+    sem = ptr_to_mem;
+    south_bound_size = ptr_to_mem + sizeof(cs1550_sem);
+    north_bound_size = south_bound_size + 1;
+    flag_person_pid = north_bound_size + 1;
+    north_bound_pid = flag_person_pid + 1;
+    south_bound_pid = north_bound_pid + 1;
+    north_bound_first = (Car *) (south_bound_pid + sizeOfCar);
+    north_bound_last = north_bound_first + sizeOfCar;
+    south_bount_first = north_bound_last + sizeOfCar;
+    south_bount_last = south_bount_first + sizeOfCar;
+    south_bound_queue = south_bount_last + sizeOfCar;
+    north_bound_queue = south_bound_queue + (sizeOfCar * 10);
+}
 
 /**
  * Used to initialize the simulaiton
@@ -69,11 +101,13 @@ void up(cs1550_sem *sem) {
  */
 void init_sim() {
     //Initialize memory space
-
+    int N = calculate_mem_size();
+    void* ptr = mmap(NULL, N, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0, 0);
+    init_ptrs(ptr);
     //Initialize processes.
-    flag_person_pid = fork();
-    south_bound_pid = fork();
-    north_bound_pid = fork();
+    *flag_person_pid = fork();
+    *south_bound_pid = fork();
+    *north_bound_pid = fork();
 }
 
 /**
@@ -97,6 +131,7 @@ void delay_20_sec() {
 }
 
 void let_car_through() {
+    //Remove car from either Northbound or Southbound queues.
     //Each car takes 2 seconds to go through the construction area.
     //Sleep for 2 seconds to simulate this.
     sleep(2);
@@ -138,14 +173,14 @@ int main(int argc, char **argv) {
         // Do more stuff
         // call newly created down()
     } else if (current_process == south_bound_pid) {
-        //Producer code
+        //Southbound Producer code
         //generate cars
         // call newly created up()
         // Do more stuff
         // add to southbound_queue
         // call newly created down()
     } else if (current_process == north_bound_pid) {
-        //Producer code
+        //Northbound Producer code
         //generate cars
         // call newly created up()
         // Do more stuff
